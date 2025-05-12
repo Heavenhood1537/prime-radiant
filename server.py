c79c32c61eb8c23bb29e56d11bebf8945c9977d6
Heavenhood1537 <milan1537@gmail.com>
2025-05-12 09:42:33 -0700

    Added server.py for local development

---
+++ server.py
@@ -0,0 +1,15 @@
+import http.server
+import socketserver
+
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+handler_object = MyHttpRequestHandler
+PORT = 8000
+
+my_server = socketserver.TCPServer(("", PORT), handler_object)
+print(f"Server started at localhost:{PORT}")
+my_server.serve_forever() 
